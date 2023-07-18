72a564deb3bbfbe70a9982bea71eade2d9aed559
SreejaAryahi <139847701+SreejaAryahi@users.noreply.github.com>
2023-07-18 12:41:08 +0530

    Update TestingCode.java

--- src/main/java/com/digit/Test/p1/TestingCode.java
+++ src/main/java/com/digit/Test/p1/TestingCode.java
@@ -4,8 +4,6 @@ public class TestingCode {
 	public static void main(String[] args) {
 		System.out.println("Hi from Sreeja");
 		System.out.println("Hi from Sreeja");
-		System.out.println("Hi from Sreeja");
-		System.out.println("Hi from Sreeja");
 	}
 
 }
