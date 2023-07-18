3515044dda65ce9fb629c275a91cfe828c6ca2f8
Sreeja.Aryahi <Sreeja.Aryahi@LDIGITBGL-L0237.godigit.corp>
2023-07-18 12:25:55 +0530

    This is my 1st commit

--- src/main/java/com/digit/Test/p1/TestingCode.java
+++ src/main/java/com/digit/Test/p1/TestingCode.java
@@ -1,5 +1,11 @@
 package com.digit.Test.p1;
 
 public class TestingCode {
+	public static void main(String[] args) {
+		System.out.println("Hi from Sreeja");
+		System.out.println("Hi from Sreeja");
+		System.out.println("Hi from Sreeja");
+		System.out.println("Hi from Sreeja");
+	}
 
 }
